c2a122d2f203eec5aa7bb21cca57c327bf59139b
Etudiant O'clock <pierre.alexis.56@gmail.com>
2023-06-06 15:32:56 +0200

    messageman component almost dynamic with redirecct lik to network and list mapped

--- components/DisplayMessageMan.tsx
+++ components/DisplayMessageMan.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { useState } from 'react'
 import Image from "next/image"
+import Link from 'next/link'
 
 
 function DisplayMessageMan() {
@@ -17,23 +18,53 @@ function DisplayMessageMan() {
         setHover(false);
     };
 
+    const contacts = [
+        {
+            network:"Linkedin",
+            adress:"https://www.linkedin.com/in/pierre-alexis-foy/"
+        },
+        {
+            network:"GitHub",
+            adress:"https://github.com/PierrealFOY/"
+        },
+        {
+            network:"Mail",
+            adress:"pierre.alexis.56@gmail.com"
+        }
+    ]
+
     return (
-            <div className='relative flex flex-row h-90'>
+            <div 
+            className='relative flex flex-row h-90'
+            onMouseEnter={handleMouseOver}
+            onMouseLeave={handleMouseOut}
+            >
                 <Image
                     src="/manSayHiAbout.svg"
                     alt=""
                     width={310}
                     height={310}
                     className=" hidden transition-transform duration-300 transform md:block  md:top-20 md:right-8 mt-5" 
-                    onMouseOver={handleMouseOver}
-                    onMouseOut={handleMouseOut}
                 />
-                <div className='absolute top-12 left-14 translate-x-1/2 '>
+                <div className=' absolute top-5 right-12 translate-x-1/2 '>
                     {hover && (
-                        <div className='rounded-full border-4 p-3'>
-                            <h2 className='flex items-center justify-center text-black'>
-                                Si vous voulez me contacter : 0202020202
-                            </h2>
+                        <div className='overflow-hidden rounded-lg border-4 p-3'>
+                                <h2 className='text-2xl font-bold flex items-center justify-center '>
+                                    Si vous voulez me contacter :
+                                </h2>
+                                {
+                                    contacts.map((item, id) => {
+                                        return (
+                                            <Link
+                                            key={id}
+                                            href={item.adress}
+                                            className="justify-center bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                                            >
+                                                    {item.network + ' : ' + item.adress}  
+                                            </Link>
+                                        )
+                                    })
+                                }
                         </div>
                     )}
                 </div>
