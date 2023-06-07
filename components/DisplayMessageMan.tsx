3e1d69ce03160eda163be3e184616ee30358bf1c
Etudiant O'clock <pierre.alexis.56@gmail.com>
2023-06-07 11:32:38 +0200

    try to change the ternary condition to display message and image according to screen

--- components/DisplayMessageMan.tsx
+++ components/DisplayMessageMan.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 "use client"
 import React from 'react'
 import { useState } from 'react'
@@ -9,8 +10,7 @@ import { FaArrowRight } from 'react-icons/fa'
 function DisplayMessageMan() {
     const [hover, setHover ] = useState(false);
 
-    console.log('je suis là');
-
+    // hook to display a message in the bubble if image hovered or not
     const handleMouseOver = () => {
         setHover(true);
     };
@@ -19,6 +19,7 @@ function DisplayMessageMan() {
         setHover(false);
     };
 
+    // array of objects to map
     const contacts = [
         {
             network:"Linkedin",
@@ -35,51 +36,72 @@ function DisplayMessageMan() {
     ]
 
     return (
-            <div 
-                className=' mt-5 justify-center relative flex flex-row h-90'
-                onMouseEnter={handleMouseOver}
-                onMouseLeave={handleMouseOut}
-            >
-                <div className='absolute z-index'>
-                    <div className=''>
-                        <p className='pr-2 ease-in duration-300'>
-                            Passe la souris au-dessus
-                        </p>
-                        <FaArrowRight className='text-3xl'/>
-                    </div>
+        <div 
+            className='mt-5 flex flex-col md:p-4 md:flex-row '
+            onMouseEnter={handleMouseOver}
+            onMouseLeave={handleMouseOut}
+        >
+            {/* <div className='hidden absolute z-index'>
+                <div className=''>
+                    <p className='pr-2 ease-in duration-300'>
+                    </p>
+                    <FaArrowRight className='text-3xl'/>
                 </div>
+            </div> */}
+            <div className='hidden md:block md:ml-1 md:mt-2'>
                 <Image
                     src="/manSayHiAbout.svg"
                     alt=""
-                    width={310}
-                    height={310}
-                    className=" hidden transition-transform duration-300 transform md:block  md:top-20 md:right-8 mt-5" 
+                    width={300}
+                    height={300}
+                    className=" " 
                 />
-                <div className='mr-1 absolute top-5 right-12 translate-x-1/2 '>
-                    {hover && (
-                        <div className='flex flex-wrap overflow-hidden rounded-lg border-4 p-3'>
-                                <h2 className='text-2xl font-bold flex items-center justify-center '>
-                                    Si vous voulez me contacter :
-                                </h2>
-                                {
-                                    contacts.map((item, id) => {
-                                        return (
-                                            <Link
-                                            key={id}
-                                            href={item.adress}
-                                            target="_blank"
-                                            className="justify-center bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
-                                            >
-                                                    {item.network + ' : ' + item.adress}  
-                                            </Link>
-                                        )
-                                    })
-                                }
-                        </div>
-                    )}
-                </div>
             </div>
-    )
+            <div className='md:pl-3'>
+                {hover ? (
+                    <div className='hidden md:flex md:flex-wrap md:flex-col md:overflow-hidden md:rounded-lg md:border-4 md:p-3'>
+                        <h2 className='md:text-left-0 text-2xl font-bold md:flex items-center justify-center '>
+                            Pour me contacter :
+                        </h2>
+                        {
+                            contacts.map((item, id) => {
+                                return (
+                                    <Link
+                                        key={id}
+                                        href={item.adress}
+                                        target="_blank"
+                                        className="justify-center bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                                    >
+                                        {item.network + ' : ' + item.adress}  
+                                    </Link>
+                                );
+                            })
+                        }
+                    </div>
+            ) : (
+                <div className='flex flex-wrap flex-col overflow-hidden rounded-lg border-4 p-3'>
+                    <h2 className='text-left-0 text-2xl font-bold flex items-center justify-center '>
+                        Pour me contacter :
+                    </h2>
+                    {
+                        contacts.map((item, id) => {
+                            return (
+                                <Link
+                                key={id}
+                                href={item.adress}
+                                target="_blank"
+                                className="justify-center bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                                >
+                                        {item.network + ' : ' + item.adress}  
+                                </Link>
+                            );
+                        })
+                    }
+                </div>
+            )}
+        </div>
+        </div>
+    );
 }
 
 export default DisplayMessageMan
