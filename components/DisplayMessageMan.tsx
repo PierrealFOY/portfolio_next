349e3ef6318d43ac44c6f00679d36fe6b5aa3197
Etudiant O'clock <pierre.alexis.56@gmail.com>
2023-06-07 20:28:10 +0200

    link errors in display section changed in to

--- components/DisplayMessageMan.tsx
+++ components/DisplayMessageMan.tsx
@@ -49,7 +49,7 @@ function DisplayMessageMan() {
                 {contacts.map((item, id) => (
                     <Link
                         key={id}
-                        href={item.adress}
+                        to={item.adress}
                         target="_blank"
                         className="justify-center bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
                     >
@@ -76,7 +76,7 @@ function DisplayMessageMan() {
                             return (
                                 <Link
                                 key={id}
-                                href={item.adress}
+                                to={item.adress}
                                 target="_blank"
                                 className="justify-center bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
                                 >
