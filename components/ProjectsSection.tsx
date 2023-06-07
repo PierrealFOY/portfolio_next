d0b01f74f79ab6979174e07e01d08c1e7dec8308
Etudiant O'clock <pierre.alexis.56@gmail.com>
2023-06-07 16:59:32 +0200

    initial commit

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -10,7 +10,7 @@ import { RxLapTimer } from "react-icons/rx"
 
 /* eslint-disable react/no-unescaped-entities */
 
-const projects = [
+const projects = [ 
     {
         name: "What's The Movie ?",
         description: " Projet de fin d'études durant ma formation de Développeur Web et Web Mobile. Site où l'utilisateur peut jouer à un quiz sur le milieu du cinéma. Parmi 4 propositions, l'utilisateur doit trouver le bon film, un timer défile et des indices apparaissent pour l'aider. Back Office créé avec Symfony et BootStrap. Des rôles administrateurs sont créés pour gérer le site. Un utilisateur peut connecté peut ajout des films en Base De Données qui devront au préalable être validés par les administrateurs. Nous étions 6 développeurs pour coder ce site from scratch en méthode Agile pendant 4 semaines. ",
